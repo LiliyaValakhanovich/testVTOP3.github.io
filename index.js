5e9636f8537bbd1e2a114b6f77be92e857a9369f
Liliya <6750lilia@gmail.com>
2024-07-04 14:16:25 +0300

    add burger transform

--- index.js
+++ index.js
@@ -9,10 +9,12 @@ function appearBurgerMenu(event){
     console.log(true);
     burgerMenu.classList.remove("hidden");
     burgerMenu.classList.add("visible");
+    burger.classList.add("open");
     document.body.style.overflow='hidden';
   } else{
     burgerMenu.classList.remove("visible");
     burgerMenu.classList.add("hidden");
+    burger.classList.remove("open");
     document.body.style.overflow='visible';
   }
 }
